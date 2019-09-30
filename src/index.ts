25f5de53eb6842c160e000bf29b20ac6f31f4327
Leonardo Perria <leo.perria@stelltec.com>
2019-09-30 13:27:04 +0100

    First trivial implementation, single city

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import got from "got"
 import { OpenWeatherCurrent, OpenWeatherErrorBody } from "./model/open_weather"
 import { HTTP_REQUEST_RETRIES, HTTP_REQUEST_TIMEOUT } from "./config/config"
+import moment from "moment"
 
 if (fs.existsSync(".env")) {
   console.log("Using .env file to supply config environment variables")
@@ -12,29 +13,43 @@ if (fs.existsSync(".env")) {
   process.exit(1)
 }
 
+const OPENWEATHER_API_ENDPOINT = "http://api.openweathermap.org/data/2.5"
+
+async function getWeather(cityOrZipcode: string) {
+  const res = await got.get(`${OPENWEATHER_API_ENDPOINT}/weather`, {
+    json: true,
+    timeout: HTTP_REQUEST_TIMEOUT,
+    retry: HTTP_REQUEST_RETRIES,
+    query: {
+      q: cityOrZipcode,
+      units: "metric",
+      APPID: process.env.APPID
+    }
+  })
+
+  return res.body as OpenWeatherCurrent
+}
+
 (async () => {
   try {
 
-    const query = "London"
+    const query = process.argv[2]
 
     try {
-      const url = "http://api.openweathermap.org/data/2.5/weather"
-      const res = await got.get(url, {
-        json: true,
-        timeout: HTTP_REQUEST_TIMEOUT,
-        retry: HTTP_REQUEST_RETRIES,
-        query: {
-          q: query,
-          units: "metric",
-          APPID: process.env.APPID
-        }
-      })
 
-      const current = res.body as OpenWeatherCurrent
+      const weather = await getWeather(query)
 
-      console.log(JSON.stringify(current, null, 4))
+      console.log("Weather **********************************************:")
+      console.log(JSON.stringify(weather, null, 4))
+      const utcCurrentTime = moment.utc()
+
+      console.log("Time **********************************************:")
+      const cityCurrentTime = utcCurrentTime.clone().utcOffset(weather.timezone / 60)
+      console.log("cityCurrentTime=" + cityCurrentTime.format())
+
 
     } catch (error) {
+
       if (error instanceof got.RequestError) {
         console.error(`RequestError, message="${error.message}"`)
       } else if (error instanceof got.HTTPError) {
@@ -46,6 +61,7 @@ if (fs.existsSync(".env")) {
           console.error(`  message="${error.message}"`)
           console.error(`  code=${error.statusCode}`)
           console.error(`  statusMessage=${error.statusMessage}`)
+          console.error(`  url=${error.url}`)
           console.error(`  body=${errObj}`)
         }
       } else if (error instanceof got.TimeoutError) {
